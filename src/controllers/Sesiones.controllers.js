633bee42e8fb578528cd39337ce007f6cddc2f15
Angel Ruiz <aruiz@segurointeligente.mx>
2023-09-08 01:28:35 -0600

    Fix

--- src/controllers/Sesiones.controllers.js
+++ src/controllers/Sesiones.controllers.js
@@ -3,7 +3,7 @@ import { pool } from '../db.js'
 export const POSTSesion  = async (req, res) => {
     const {URL,FechaDCreacion,IP} = req.body
     try {
-    const [rows] = await pool.query('INSERT INTO Sesiones (URL,FechaDCreacion,IP) VALUES (?,?,?,?)', [nombreContacto,Puesto,Celular,Mail,Razon,RFC,NombreEmpresa,Autorizada])
+    const [rows] = await pool.query('INSERT INTO Sesiones (URL,FechaDCreacion,IP) VALUES (?,?,?)', [URL,FechaDCreacion,IP])
     //console.log(rows) 
     res.send({
         message: "Registro Exitoso",
