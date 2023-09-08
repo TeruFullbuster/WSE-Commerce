ab209bc3a4d5859a87f865b4e1282555560d87c3
Angel Ruiz <aruiz@segurointeligente.mx>
2023-09-08 01:55:53 -0600

    Add Prima Total

--- src/controllers/Sesiones.controllers.js
+++ src/controllers/Sesiones.controllers.js
@@ -1,16 +1,17 @@
 import { pool } from '../db.js'
 
 export const POSTSesion  = async (req, res) => {
-    const {URL,FechaDCreacion,IP} = req.body
+    const {URL,FechaDCreacion,IP,primaTotal} = req.body
     try {
-    const [rows] = await pool.query('INSERT INTO Sesiones (URL,FechaDCreacion,IP) VALUES (?,?,?)', [URL,FechaDCreacion,IP])
+    const [rows] = await pool.query('INSERT INTO Sesiones (URL,FechaDCreacion,IP,primaTotal) VALUES (?,?,?,?)', [URL,FechaDCreacion,IP,primaTotal])
     //console.log(rows) 
     res.send({
         message: "Registro Exitoso",
         id: rows.insertId,
         URL,
         FechaDCreacion,
-        IP
+        IP,
+        primaTotal
     })
     } catch (error) {
         return res.status(500).json({
