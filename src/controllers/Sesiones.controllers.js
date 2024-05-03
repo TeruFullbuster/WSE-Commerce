a21288049175cfbc97e2f2ebb1400bcecc92353e
GrupoMAG <soporte@segurointeligente.mx>
2024-05-03 17:29:23 -0600

    Fix Envio

--- src/controllers/Sesiones.controllers.js
+++ src/controllers/Sesiones.controllers.js
@@ -109,7 +109,7 @@ function EnviarMail(Email , Origen) {
     const Resource = ObtenerResource(Origen)
     const raw = JSON.stringify({
     "DATOSHERRAMIENTAS": {
-        "Destinatario": "aruiz@segurointeligente.mx",
+        "Destinatario": Email,
         "MensajeCorreo": `<html lang=\"es\"><head><meta charset=\"utf-8\"><meta http-equiv=\"X-UA-Compatible\" 
         content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>
         <body style=\"width: 600px; text-align: center; margin: 25px auto; font-family: sans-serif;\">
