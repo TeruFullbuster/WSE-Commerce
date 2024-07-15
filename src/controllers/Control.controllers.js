aa28005033ed81517bf0c2051ef44fa7914a2614
GrupoMAG <soporte@segurointeligente.mx>
2024-07-15 13:02:51 -0600

    Error fecha

--- src/controllers/Control.controllers.js
+++ src/controllers/Control.controllers.js
@@ -4,10 +4,10 @@ import fetch from 'node-fetch';
 export const LogPMP  = async (req, res) => {
 
     const {correo,LogsPMP} = req.body
-
+    console.log(correo,LogsPMP)
     try {
-    const [rows] = await pool.query('INSERT INTO LogsPMP (correo,LogsPMP) VALUES (?,?)', [correo,LogsPMP])
-    //console.log(rows) 
+    const [rows] = await pool.query('INSERT INTO LogsPMP (correo,fechalogs) VALUES (?,?)', [correo,LogsPMP])
+    console.log(rows) 
     res.send({
         message: "Registro Exitoso"        
     })
